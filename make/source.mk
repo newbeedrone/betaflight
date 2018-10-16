c24344b859f8c22b9ea0ba16a3095f2976d3707c
yangye1234 <1174032506@qq.com>
2018-10-16 12:18:06 +0800

    add beesign driver file

--- make/source.mk
+++ make/source.mk
@@ -138,6 +138,7 @@ COMMON_SRC = \
             cms/cms_menu_osd.c \
             cms/cms_menu_power.c \
             cms/cms_menu_saveexit.c \
+            cms/cms_menu_vtx_beesign.c\
             cms/cms_menu_vtx_rtc6705.c \
             cms/cms_menu_vtx_smartaudio.c \
             cms/cms_menu_vtx_tramp.c \
@@ -178,6 +179,7 @@ COMMON_SRC = \
             telemetry/ibus_shared.c \
             sensors/esc_sensor.c \
             io/vtx.c \
+            io/vtx_beesign.c \
             io/vtx_rtc6705.c \
             io/vtx_smartaudio.c \
             io/vtx_tramp.c \
@@ -327,6 +329,7 @@ SIZE_OPTIMISED_SRC := $(SIZE_OPTIMISED_SRC) \
             cms/cms_menu_osd.c \
             cms/cms_menu_power.c \
             cms/cms_menu_saveexit.c \
+            cms/cms_menu_vtx_beesign.c\
             cms/cms_menu_vtx_rtc6705.c \
             cms/cms_menu_vtx_smartaudio.c \
             cms/cms_menu_vtx_tramp.c \
