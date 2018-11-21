2ff08e87dcf75709d3a6c7c09d61f7e432f1a668
czchc <chenhaicong@hotmail.com>
2018-11-21 10:01:12 +0800

    add vtx lock freq
    update vtx lock to support betaflight 4.0

--- src/main/cms/cms_menu_vtx_rtc6705.c
+++ src/main/cms/cms_menu_vtx_rtc6705.c
@@ -40,9 +40,18 @@
 #include "io/vtx_rtc6705.h"
 #include "io/vtx.h"
 
+#if defined(USE_VTX_LOCK_FREQ)
+#define VTX_FREQ_ISM_MAX                5920
+#define VTX_FREQ_ISM_MIN                5650
+#endif // USE_VTX_LOCK_FREQ
+
 static uint8_t cmsx_vtxBand;
 static uint8_t cmsx_vtxChannel;
 static uint8_t cmsx_vtxPower;
+#if defined(USE_VTX_LOCK_FREQ)
+static uint8_t lastBand;
+static uint8_t lastChannel;
+#endif // USE_VTX_LOCK_FREQ
 
 static OSD_TAB_t entryVtxBand;
 static OSD_TAB_t entryVtxChannel;
@@ -52,6 +61,10 @@ static void cmsx_Vtx_ConfigRead(void)
 {
     vtxCommonGetBandAndChannel(vtxCommonDevice(), &cmsx_vtxBand, &cmsx_vtxChannel);
     vtxCommonGetPowerIndex(vtxCommonDevice(), &cmsx_vtxPower);
+#if defined(USE_VTX_LOCK_FREQ)
+    lastBand = cmsx_vtxBand;
+    lastChannel = cmsx_vtxChannel;
+#endif // USE_VTX_LOCK_FREQ
 }
 
 static void cmsx_Vtx_ConfigWriteback(void)
@@ -64,6 +77,69 @@ static void cmsx_Vtx_ConfigWriteback(void)
 
     saveConfigAndNotify();
 }
+
+#if defined(USE_VTX_LOCK_FREQ)
+static uint8_t cms_Vtx_Check_Lock_Freq(uint16_t freq) {
+    if (freq < VTX_FREQ_ISM_MIN || freq > VTX_FREQ_ISM_MAX) {
+        return 1;
+    }
+    return 0;
+}
+
+static long cmsx_Vtx_Band_Lock_Check(displayPort_t *pDisp, const void *self) {
+    UNUSED(pDisp);
+    UNUSED(self);
+    if (cmsx_vtxBand == 0) {
+        cmsx_vtxBand = 1;
+    }
+    if (cms_Vtx_Check_Lock_Freq(vtxCommonLookupFrequency(vtxCommonDevice(), cmsx_vtxBand + 1, cmsx_vtxChannel))) {
+        cmsx_vtxChannel = 1;
+    }
+    while (1) {
+        if (cms_Vtx_Check_Lock_Freq(vtxCommonLookupFrequency(vtxCommonDevice(), cmsx_vtxBand + 1, cmsx_vtxChannel))) {
+            cmsx_vtxChannel += 1;
+            if (cmsx_vtxChannel > VTX_SETTINGS_CHANNEL_COUNT - 1) {
+                if (lastBand - cmsx_vtxBand >= 1) {
+                    cmsx_vtxBand -= 1;
+                    cmsx_vtxChannel = 1;
+                } else {
+                    cmsx_vtxBand += 1;
+                    cmsx_vtxChannel = 1;
+                }
+            }
+        } else {
+            lastBand = cmsx_vtxBand;
+            break;
+        }
+    }
+    return 0;
+}
+
+static long cmsx_Vtx_Channel_Lock_Check(displayPort_t *pDisp, const void *self) {
+    UNUSED(pDisp);
+    UNUSED(self);
+    if (cmsx_vtxChannel == 0) {
+        cmsx_vtxChannel = 1;
+    }
+    while (1) {
+        if (cms_Vtx_Check_Lock_Freq(vtxCommonLookupFrequency(vtxCommonDevice(), cmsx_vtxBand + 1, cmsx_vtxChannel))) {
+            if (lastChannel - cmsx_vtxChannel >= 1) {
+                cmsx_vtxChannel -= 1;
+            } else {
+                cmsx_vtxChannel += 1;
+            }
+            if (cmsx_vtxChannel == 0 || cmsx_vtxChannel == 9) {
+                cmsx_vtxChannel = lastChannel;
+                break;
+            }
+        } else {
+            lastChannel = cmsx_vtxChannel;
+            break;
+        }
+    }
+    return 0;
+}
+#endif // USE_VTX_LOCK_FREQ
 
 static long cmsx_Vtx_onEnter(void)
 {
@@ -125,8 +201,13 @@ static long cmsx_Vtx_onPowerChange(displayPort_t *pDisp, const void *self)
 
 static const OSD_Entry cmsx_menuVtxEntries[] = {
     {"--- VTX ---", OME_Label, NULL, NULL, 0},
+#if defined(USE_VTX_LOCK_FREQ)
+    {"BAND", OME_TAB, cmsx_Vtx_Band_Lock_Check, &entryVtxBand, DYNAMIC},
+    {"CHANNEL", OME_UINT8, cmsx_Vtx_Channel_Lock_Check, &entryVtxChannel, DYNAMIC},
+#else
     {"BAND", OME_TAB, cmsx_Vtx_onBandChange, &entryVtxBand, 0},
     {"CHANNEL", OME_TAB, cmsx_Vtx_onChanChange, &entryVtxChannel, 0},
+#endif // USE_VTX_LOCK_FREQ
     {"POWER", OME_TAB, cmsx_Vtx_onPowerChange, &entryVtxPower, 0},
     {"BACK", OME_Back, NULL, NULL, 0},
     {NULL, OME_END, NULL, NULL, 0}
