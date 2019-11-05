06d9670163726dea02ffb9e476cbb238a6506291
yangyeah <1174032506@qq.com>
2019-11-05 12:14:32 +0800

    bl vtxtable

--- src/main/target/BEEBRAIN_BL/config.c
+++ src/main/target/BEEBRAIN_BL/config.c
@@ -50,6 +50,7 @@
 #include "pg/vcd.h"
 #include "pg/rx.h"
 #include "pg/motor.h"
+#include "pg/vtx_table.h"
 
 #include "rx/rx.h"
 #include "rx/cc2500_frsky_common.h"
@@ -112,6 +113,62 @@ void targetConfiguration(void)
 
     vtxSettingsConfigMutable()->band = 5;
     vtxSettingsConfigMutable()->channel = 8;
+    vtxSettingsConfigMutable()->power = 2;
+#if (defined(MOCKINGBIRD_DSM_US) || defined(MOCKINGBIRD_FRSKY_US) || defined(MOCKINGBIRD_SFHSS_US))
+    uint16_t vtxTableFrequency[6][8] = {
+        { 5865, 5845, 5825, 5805, 5785, 5765, 5745, 5725 }, // Boscam A
+        { 5733, 5752, 5771, 5790, 5809, 5828, 5847, 5866 }, // Boscam B
+        { 5705, 5685, 5665,    0, 5885, 5905,    0,    0 }, // Boscam E
+        { 5740, 5760, 5780, 5800, 5820, 5840, 5860, 5880 }, // FatShark
+        { 5658, 5695, 5732, 5769, 5806, 5843, 5880, 5917 }, // RaceBand
+        { 5732, 5765, 5828, 5840, 5866, 5740,    0,    0 }, // IMD6
+    };
+#else 
+    uint16_t vtxTableFrequency[6][8] = {
+        { 5865, 5845, 5825, 5805, 5785, 5765, 5745, 5725 }, // Boscam A
+        { 5733, 5752, 5771, 5790, 5809, 5828, 5847, 5866 }, // Boscam B
+        { 5705, 5685, 5665, 5645, 5885, 5905, 5925, 5945 }, // Boscam E
+        { 5740, 5760, 5780, 5800, 5820, 5840, 5860, 5880 }, // FatShark
+        { 5658, 5695, 5732, 5769, 5806, 5843, 5880, 5917 }, // RaceBand
+        { 5732, 5765, 5828, 5840, 5866, 5740,    0,    0 }, // IMD6
+    };
+#endif
+    const char * vtxTableBandNames[6] = {
+            "BOSCAM A",
+            "BOSCAM B",
+            "BOSCAM E",
+            "FATSHARK",
+            "RACEBAND",
+            "IMD6"
+    };
+    char vtxTableBandLetters[7] = "ABEFRI";
+    vtxTableConfigMutable()->bands = 6;
+    vtxTableConfigMutable()->channels = 8;
+    for (uint8_t i = 0; i < 6; i++) {
+        for (uint8_t j = 0; j < 8; j++) {
+            vtxTableConfigMutable()->frequency[i][j] = vtxTableFrequency[i][j];
+        }
+    }
+    for (uint8_t i = 0; i < 6; i++) {
+        strcpy(vtxTableConfigMutable()->bandNames[i], vtxTableBandNames[i]);
+        vtxTableConfigMutable()->bandLetters[i] = vtxTableBandLetters[i];
+    }
+    strcpy(vtxTableConfigMutable()->channelNames[0], "1");
+    strcpy(vtxTableConfigMutable()->channelNames[1], "2");
+    strcpy(vtxTableConfigMutable()->channelNames[2], "3");
+    strcpy(vtxTableConfigMutable()->channelNames[3], "4");
+    strcpy(vtxTableConfigMutable()->channelNames[4], "5");
+    strcpy(vtxTableConfigMutable()->channelNames[5], "6");
+    strcpy(vtxTableConfigMutable()->channelNames[6], "7");
+    strcpy(vtxTableConfigMutable()->channelNames[7], "8");
+    vtxTableConfigMutable()->powerLevels = 3;
+    vtxTableConfigMutable()->powerValues[0] = 0;
+    vtxTableConfigMutable()->powerValues[1] = 1;
+    vtxTableConfigMutable()->powerValues[2] = 2;
+    strcpy(vtxTableConfigMutable()->powerLabels[0], "5");
+    strcpy(vtxTableConfigMutable()->powerLabels[1], "25");
+    strcpy(vtxTableConfigMutable()->powerLabels[2], "100");
+    
 
     // batteryConfigMutable()->batteryCapacity = 250;
     // batteryConfigMutable()->vbatmincellvoltage = 28;
