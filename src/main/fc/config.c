72f04407ae025ba77c0d9464412078a077c059c8
yangyeah <1174032506@qq.com>
2019-11-05 12:18:38 +0800

    fc bmi160 config

--- src/main/fc/config.c
+++ src/main/fc/config.c
@@ -597,6 +597,8 @@ void validateAndFixGyroConfig(void)
         break;
     case BMI_160_SPI:
         samplingTime = 0.0003125f;
+        gyroConfigMutable()->gyro_sync_denom = 2;
+        pidConfigMutable()->pid_process_denom = 1;
         break;
     default:
         samplingTime = 0.000125f;
@@ -606,6 +608,11 @@ void validateAndFixGyroConfig(void)
         switch (gyroMpuDetectionResult()->sensor) {
         case ICM_20649_SPI:
             samplingTime = 1.0f / 1100.0f;
+            break;
+        case BMI_160_SPI:
+            samplingTime = 0.0003125f;
+            gyroConfigMutable()->gyro_sync_denom = 2;
+            pidConfigMutable()->pid_process_denom = 1;
             break;
         default:
             samplingTime = 0.001f;
