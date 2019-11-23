34537a4d85e0896c6114daec3df2027d4797b9b6
yangyeah <1174032506@qq.com>
2019-11-23 19:01:40 +0800

    change to use bmi160

--- src/main/target/BEEBRAIN_PRO/target.mk
+++ src/main/target/BEEBRAIN_PRO/target.mk
@@ -2,6 +2,7 @@ F411_TARGETS += $(TARGET)
 FEATURES     = VCP ONBOARDFLASH
 
 TARGET_SRC  =                                       \
+            drivers/accgyro/accgyro_spi_bmi160.c \
              drivers/accgyro/accgyro_spi_mpu6500.c \
              drivers/accgyro/accgyro_mpu6500.c \
              drivers/accgyro/accgyro_spi_mpu6000.c  \
