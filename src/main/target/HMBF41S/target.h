d04ea0649bb16118e8cfb70e0077459e230d4077
yangyeah <1174032506@qq.com>
2020-02-22 17:18:38 +0800

    add undefine use tramp and smartaudio

--- src/main/target/HMBF41S/target.h
+++ src/main/target/HMBF41S/target.h
@@ -104,6 +104,8 @@
 #define MAX7456_SPI_INSTANCE            SPI3
 #define MAX7456_SPI_CS_PIN              SPI3_NSS_PIN
 
+#undef USE_VTX_TRAMP
+#undef USE_VTX_SMARTAUDIO
 #define USE_BEESIGN
 // #define USE_OSD_BEESIGN
 #define USE_BEESIGN_UART                SERIAL_PORT_USART1
